fccb4b211ad3a79a5d075dfb3b41f96af646371a
RomanLukanin <roman.lukanin@aplana.com>
2017-12-11 17:11:15 +0300

    Операции с ++

--- src/main/java/ru/aplana/lesson/Lesson2.java
+++ src/main/java/ru/aplana/lesson/Lesson2.java
@@ -4,7 +4,7 @@ public class Lesson2 {
     public static void main(String[] args)
     {
         int i = 2;
-        i += i++ + ++i;
+        i += i++ + ++i + i++;
         System.out.println(i);
 
         //System.out.println("asdfasdf");
